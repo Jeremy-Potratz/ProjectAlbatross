7a48973fce15154c1d5c87d9c35ed4d33873d487
Scott <Scott@Scotts-MacBook-Pro.local>
2017-03-11 17:31:47 -0600

    Hid status bar

--- ProjectAlbatross/ProjectAlbatross/AppDelegate.swift
+++ ProjectAlbatross/ProjectAlbatross/AppDelegate.swift
@@ -17,6 +17,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
         window?.rootViewController = TabController()
+        application.isStatusBarHidden = true
         window?.makeKeyAndVisible()
         // Override point for customization after application launch.
         return true
