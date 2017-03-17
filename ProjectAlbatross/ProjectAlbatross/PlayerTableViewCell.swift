743a621b9d565b35a0954949c1c8cfd6b3fa34d7
spogatetz8386 <spogatetz8386@stu.d214.org>
2017-03-17 13:52:12 -0500

    Built folding cell

---
+++ ProjectAlbatross/ProjectAlbatross/PlayerTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  PlayerTableViewCell.swift
+//  ProjectAlbatross
+//
+//  Created by apcsp on 3/17/17.
+//  Copyright © 2017 apcsp. All rights reserved.
+//
+
+import UIKit
+
+class PlayerTableViewCell: FoldingCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
